9e77641e6f1ebbe6fe30782255836003a421f6b2
sneprokin <neprokinalex94@gmail.com>
2020-07-03 14:12:31 +0300

    adding an initial project

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBox 
-        placeholder='search robos' 
+        placeholder='search monsters' 
         handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}> 
