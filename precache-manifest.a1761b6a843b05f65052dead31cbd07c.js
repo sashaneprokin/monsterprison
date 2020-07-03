4920f82df37268350c15756bcf7752118449ea5e
sneprokin <neprokinalex94@gmail.com>
2020-07-03 15:17:50 +0300

    Updates

---
+++ precache-manifest.a1761b6a843b05f65052dead31cbd07c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b97d17e950a193663ea006b1cf22e5",
+    "url": "/monsterprison/index.html"
+  },
+  {
+    "revision": "32176d711675725a0851",
+    "url": "/monsterprison/static/css/main.01a3fac0.chunk.css"
+  },
+  {
+    "revision": "2c8b81a9519d3c3d90a0",
+    "url": "/monsterprison/static/js/2.5f093af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterprison/static/js/2.5f093af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32176d711675725a0851",
+    "url": "/monsterprison/static/js/main.fc655405.chunk.js"
+  },
+  {
+    "revision": "e468f000916f50043051",
+    "url": "/monsterprison/static/js/runtime-main.496b3b12.js"
+  }
+]);
